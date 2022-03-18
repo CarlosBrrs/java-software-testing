b1981e0c1e392121e54b605789701d3b5d73c615
Carlos Barrios Pereira <caebarri@bancolombia.com.co>
2022-03-18 02:05:29 -0500

    Adding customer folder with controller, entity and repo classes

---
+++ src/main/java/com/amigoscode/testing/customer/services/ICustomerRegistrationService.java
@@ -0,0 +1,7 @@
+package com.amigoscode.testing.customer.services;
+
+import com.amigoscode.testing.customer.model.domain.CustomerRegistrationRequest;
+
+public interface ICustomerRegistrationService {
+
+}
